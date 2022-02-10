b37ca49fe3b12df398afb52f18a69da4ce5d97fa
shota-aa <dqx1005@icloud.com>
2022-02-10 10:48:22 +0900

    add  components/Transaction

--- src/pages/Transactions.vue
+++ src/pages/Transactions.vue
@@ -1,11 +1,63 @@
 <script lang="ts" setup>
-import Request from '../components/Transaction.vue'
+import Transaction from '../components/Transaction.vue'
 const transactions = [
   {
     id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
     amount: 1200,
     target: 'hoge株式会社',
     tags: [
+      {
+        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        name: '2020講習会',
+        description: '2020年度講習会',
+        created_at: '2022-02-09T14:03:53.278Z',
+        updated_at: '2022-02-09T14:03:53.278Z'
+      },
+      {
+        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        name: '2020講習会',
+        description: '2020年度講習会',
+        created_at: '2022-02-09T14:03:53.278Z',
+        updated_at: '2022-02-09T14:03:53.278Z'
+      },
+      {
+        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        name: '2020講習会',
+        description: '2020年度講習会',
+        created_at: '2022-02-09T14:03:53.278Z',
+        updated_at: '2022-02-09T14:03:53.278Z'
+      }
+    ],
+    group: {
+      id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+      name: 'SysAd',
+      description: 'SysAd班',
+      budget: 250000,
+      created_at: '2022-02-09T14:03:53.278Z',
+      updated_at: '2022-02-09T14:03:53.278Z'
+    },
+    created_at: '2022-02-09T14:03:53.278Z',
+    updated_at: '2022-02-09T14:03:53.278Z'
+  },
+  {
+    id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+    amount: 1200,
+    target: 'hoge株式会社',
+    tags: [
+      {
+        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        name: '2020講習会',
+        description: '2020年度講習会',
+        created_at: '2022-02-09T14:03:53.278Z',
+        updated_at: '2022-02-09T14:03:53.278Z'
+      },
+      {
+        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+        name: '2020講習会',
+        description: '2020年度講習会',
+        created_at: '2022-02-09T14:03:53.278Z',
+        updated_at: '2022-02-09T14:03:53.278Z'
+      },
       {
         id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
         name: '2020講習会',
@@ -30,18 +82,18 @@ const transactions = [
 <template>
   <dev class="flex justify-evenly">
     <dev class="min-w-80 min-h-80 text-center border-3">aaaa</dev>
-    <dev class="border-3">
+    <dev class="border-3 min-w-150 w-3/5">
       <h1 class="text-center border">入出金記録</h1>
-      <dev class="min-w-175 text-center border flex justify-around">
-        <dev>年 月 日</dev>
-        <dev>取引額</dev>
-        <dev>取引相手</dev>
-        <dev>取引グループ</dev>
-        <dev>タグ</dev>
+      <dev class="text-center border flex">
+        <dev class="w-1/6">年 月 日</dev>
+        <dev class="w-1/6">取引額</dev>
+        <dev class="w-1/6">取引相手</dev>
+        <dev class="w-1/6">取引グループ</dev>
+        <dev class="w-1/3">タグ</dev>
       </dev>
       <ul>
         <li v-for="transaction in transactions" :key="transaction.id">
-          <Transition :transaction="transaction" />
+          <Transaction :transaction="transaction" />
         </li>
       </ul>
     </dev>

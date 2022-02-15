1e70a47e1d9af41ca8c4202577085fa2308321bc
mehm8128 <mehm8128@gmail.com>
2022-02-15 09:51:17 +0900

    add post file api

--- src/stores/file.ts
+++ src/stores/file.ts
@@ -13,12 +13,24 @@ export const useFileStore = defineStore('file', {
   actions: {
     async getFile(ids: string[]) {
       for (let i = 0; i < ids.length; i++) {
-        const response: File = await axios.get('/api/file/' + ids[i])
+        const response: File = await axios.get('/api/files/' + ids[i])
         this.files.concat(response)
       }
     },
+    async postFile(request_id: string, name: string, file: Blob) {
+      const formData = new FormData()
+      formData.append('file', file)
+      formData.append('name', name)
+      formData.append('request_id', request_id)
+      const header = {
+        headers: {
+          'Content-Type': 'mutipart/form-data'
+        }
+      }
+      await axios.post('/api/files/', formData, header)
+    },
     async deleteFile(id: string) {
-      await axios.put('/api/file/' + id)
+      await axios.put('/api/files/' + id)
     }
   }
 })

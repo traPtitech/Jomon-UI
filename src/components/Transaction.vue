0a61c923c94bfcfc8a6db4653ac1012a8c99956f
shota-aa <dqx1005@icloud.com>
2022-03-07 22:07:55 +0900

    fix components/transaction

--- src/components/Transaction.vue
+++ src/components/Transaction.vue
@@ -1,61 +1,31 @@
 <script lang="ts" setup>
-import { computed } from 'vue'
-
-type Transaction = {
-  id: string
-  amount: number
-  target: string
-  tags: {
-    id: string
-    name: string
-    description: string
-    created_at: string
-    updated_at: string
-  }[]
-  group: {
-    id: string
-    name: string
-    description: string
-    budget: number
-    created_at: string
-    updated_at: string
-  }
-  created_at: string
-}
-type Props = { transaction: Transaction }
+import { storeToRefs } from 'pinia'
+import { useTransactionStore } from '../stores/transaction'
+const transactionStore = useTransactionStore()
+const { transactions, dateFormatter } = storeToRefs(transactionStore)
+type Props = { index: number }
 const props = defineProps<Props>()
-const dateFormatter = computed(
-  () => (date: string) =>
-    date.split('T')[0].split('-')[0] +
-    '年' +
-    date.split('T')[0].split('-')[1] +
-    '月' +
-    date.split('T')[0].split('-')[2] +
-    '日'
-)
 </script>
 
 <template>
-  <router-link :to="'/transaction/' + props.transaction.id">
-    <dev class="flex">
-      <dev class="w-1/6 text-center">
-        {{ dateFormatter(props.transaction.created_at) }}
-      </dev>
-      <dev class="w-1/6 text-center">{{ props.transaction.amount }}</dev>
-      <dev class="w-1/6 text-center">{{ props.transaction.target }}</dev>
-      <dev class="w-1/6 text-center">
-        {{ props.transaction.group.description }}
-      </dev>
-      <dev class="w-1/3 text-center">
-        <!-- TODO: ,最後消す -->
-        <dev
-          v-for="tag in props.transaction.tags"
-          :key="tag.id"
-          class="inline-block"
-        >
-          {{ tag.description }},&nbsp;&nbsp;
-        </dev>
+  <dev class="flex">
+    <dev class="w-1/6 text-center">
+      {{ dateFormatter(transactions[props.index].created_at) }}
+    </dev>
+    <dev class="w-1/6 text-center">{{ transactions[props.index].amount }}</dev>
+    <dev class="w-1/6 text-center">{{ transactions[props.index].target }}</dev>
+    <dev class="w-1/6 text-center">
+      {{ transactions[props.index].group.description }}
+    </dev>
+    <dev class="w-1/3 text-center">
+      <!-- TODO: ,最後消す -->
+      <dev
+        v-for="tag in transactions[props.index].tags"
+        :key="tag.id"
+        class="inline-block"
+      >
+        {{ tag.description }},&nbsp;&nbsp;
       </dev>
     </dev>
-  </router-link>
+  </dev>
 </template>

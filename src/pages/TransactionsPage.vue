d256984d5592ebb746a9dc7f421ffd5bf27bcf18
shota-aa <dqx1005@icloud.com>
2022-03-08 18:32:57 +0900

    fix trasactions table

--- src/pages/TransactionsPage.vue
+++ src/pages/TransactionsPage.vue
@@ -1,27 +1,42 @@
 <script lang="ts" setup>
 import { storeToRefs } from 'pinia'
 import { useRoute } from 'vue-router'
+import { onMounted } from 'vue'
+import { BookOpenIcon, PlusCircleIcon } from '@heroicons/vue/solid'
 
 import PaginationBar from '../components/PaginationBar.vue'
 import Transaction from '../components/Transaction.vue'
 // import NewTransactionModal from '../components/NewTransactionModal.vue'
 import { useTransactionStore } from '../stores/transaction'
+import FilteringTransactionMenu from '../components/FilteringTransactionMenu.vue'
 
 const route = useRoute()
 const pageIndex = Number(route.query.pageIndex)
 const transactionStore = useTransactionStore()
 const { isModalOpen } = storeToRefs(transactionStore)
+
+onMounted(() => {
+  transactionStore.getTransactions()
+})
 function changeIsModalOpen() {
   isModalOpen.value = !isModalOpen.value
 }
 </script>
 
 <template>
-  <!-- <NewTransactionModal v-if="isModalOpen" /> -->
-  <div class="flex">
-    <div class="font-bold text-2xl">入出金記録</div>
-    <div>新規作成</div>
+  <div v-if="isModalOpen">aaaaaaaa</div>
+  <div class="h-20 relative">
+    <BookOpenIcon class="w-12 ml-40 mt-4" />
+    <span class="font-bold text-2xl absolute top-6">入出金記録</span>
+    <PlusCircleIcon
+      class="w-10 float-right mr-40 mt-10"
+      @click="changeIsModalOpen"
+    />
   </div>
+  <div v-if="transactionStore.transactionsLength() !== 0" class="ml-30">
+    {{ transactionStore.transactionsLength() }}件取得しました
+  </div>
+  <div v-else class="ml-30">条件に一致する申請は見つかりませんでした</div>
   <div
     :class="
       pageIndex === Math.ceil(transactionStore.transactionsLength() / 13)
@@ -33,13 +48,7 @@ function changeIsModalOpen() {
       v-if="transactionStore.transactionsLength() !== 0"
       class="min-w-150 w-5/6 m-auto"
     >
-      <div class="text-center flex">
-        <div class="w-3/20">年 月 日</div>
-        <div class="w-3/20">取引額</div>
-        <div class="w-3/20">取引相手</div>
-        <div class="w-3/20">取引グループ</div>
-        <div class="w-2/5">タグ</div>
-      </div>
+      <FilteringTransactionMenu />
       <div class="border border-gray-400 border-1"></div>
       <ul>
         <li

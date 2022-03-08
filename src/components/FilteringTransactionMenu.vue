3dd1ce20290f796d4e8b34d71fd0fe8598f61205
shota-aa <dqx1005@icloud.com>
2022-03-08 18:32:57 +0900

    fix trasactions table

---
+++ src/components/FilteringTransactionMenu.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+import { SearchIcon } from '@heroicons/vue/solid'
+import { XIcon } from '@heroicons/vue/outline'
+</script>
+<template>
+  <div class="text-center flex">
+    <div class="w-3/20">年 月 日</div>
+    <div class="w-3/20">取引額</div>
+    <div class="w-3/20">取引相手</div>
+    <div class="w-3/20">取引グループ</div>
+    <div class="w-2/5">タグ</div>
+  </div>
+</template>

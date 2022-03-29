cd609bcc8872275196aac6317140237f636692fb
mehm8128 <mehm8128@gmail.com>
2022-03-29 14:26:16 +0900

    fix: change pencilIcon design

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,18 @@
-import vue from '@vitejs/plugin-vue'
 import * as https from 'https'
 import * as path from 'path'
 import brotli from 'rollup-plugin-brotli'
 import { defineConfig } from 'vite'
 import WindiCSS from 'vite-plugin-windicss'
 
+import vue from '@vitejs/plugin-vue'
+
 const keepAliveAgent = new https.Agent({ keepAlive: true })
-const DEV_SERVER_PROXY_HOST = 'https://jomon-dev.tokyotech.org'
 
 export default defineConfig({
   plugins: [vue(), brotli(), WindiCSS()],
   resolve: {
     alias: {
       '/@': path.resolve(__dirname, '/src')
-    }
-  },
-  server: {
-    open: true,
-    proxy: {
-      '/api/*': {
-        target: DEV_SERVER_PROXY_HOST,
-        changeOrigin: true,
-        agent: keepAliveAgent
-      }
     }
   }
 })

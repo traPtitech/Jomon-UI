be80990b9a6a3ed5222c58ed8d27d8a361b15750
mehm8128 <mehm8128@gmail.com>
2022-03-29 14:26:16 +0900

    fix: change pencilIcon design

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import { defineConfig } from 'vite'
 import WindiCSS from 'vite-plugin-windicss'
 
 const keepAliveAgent = new https.Agent({ keepAlive: true })
-const DEV_SERVER_PROXY_HOST = 'https://jomon-dev.tokyotech.org'
 
 export default defineConfig({
   plugins: [vue(), brotli(), WindiCSS()],

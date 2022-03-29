54d86a2d0ccd3e2bf97ac08d47f42d92fa6ad139
mehm8128 <mehm8128@gmail.com>
2022-03-29 14:59:12 +0900

    add tag component

---
+++ src/components/Tags.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { TagResponse } from '../types/tagTypes'
+
+type Props = { tags: TagResponse[]; limit: number } //1-indexedでlimitを指定
+const props = withDefaults(defineProps<Props>(), { limit: 20 })
+</script>
+<template>
+  <span
+    v-for="(tag, index) in tags.slice(
+      0,
+      !(tags.length > limit) ? tags.length : limit
+    )"
+    :key="tag.id"
+    :class="index !== 0 ? 'ml-2' : ''"
+    class="border border-solid border-black rounded"
+    >{{ tag.name }}</span
+  >
+  <span v-if="limit !== 0 && tags.length > limit"> ...</span>
+</template>

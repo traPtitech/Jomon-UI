2ae580e9fd0773671032721ea1175988dc5f25c9
mehm8128 <mehm8128@gmail.com>
2022-04-03 14:56:34 +0900

    fix tag

--- src/components/NewTagModal.vue
+++ src/components/NewTagModal.vue
@@ -1,38 +1,34 @@
 <script lang="ts" setup>
+import axios from 'axios'
 import { ref } from 'vue'
 
 import { useTagStore } from '../stores/tag'
+import Button from './Button.vue'
 
 const tagStore = useTagStore()
-const tag = ref({
-  name: '',
-  description: ''
-})
+const tagName = ref('')
+
+async function postTagAPI(tag: string) {
+  await axios.post('/api/tags', tag)
+  tagStore.getTags()
+}
 function postTag() {
-  tagStore.postTag(tag.value)
+  postTagAPI(tagName.value)
 }
 </script>
 
 <template>
-  <div class="bg-white w-200 h-100 absolute z-5 inset-0 m-auto">
+  <div class="bg-white w-160 h-80 absolute z-5 inset-0 m-auto">
     <h1 class="text-3xl text-center mt-4 mb-4">タグの新規作成</h1>
-    <div class="flex flex-col justify-around ml-12 text-xl h-4/5">
+    <div class="flex flex-col justify-around ml-12 mr-12 text-xl h-4/5">
       <div>
         <span>タグの名前：</span>
-        <input
-          v-model="tag.name"
-          class="border border-solid border-black w-2/3"
-        />
-      </div>
-      <div>
-        <span class="align-top">詳細：</span>
-        <textarea
-          v-model="tag.description"
-          class="h-32 leading-tight border border-solid border-black resize-none w-2/3"
-        />
+        <input v-model="tagName" class="border border-gray-300 w-2/3" />
       </div>
       <div class="text-center">
-        <button @click="postTag">タグを作成する</button>
+        <Button @onClick="postTag" fontSize="xl" padding="sm" class="w-48 mb-4">
+          申請を作成する</Button
+        >
       </div>
     </div>
   </div>
--- src/stores/tag.ts
+++ src/stores/tag.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { defineStore } from 'pinia'
 
-import { Tag, TagResponse, TagsResponse } from '../types/tagTypes'
+import { TagResponse, TagsResponse } from '../types/tagTypes'
 
 export const useTagStore = defineStore('tag', {
   state: () => ({
@@ -9,21 +9,18 @@ export const useTagStore = defineStore('tag', {
       {
         id: '3fa85f64-5717-4562-b3fc-2c963f66afa5',
         name: '2020講習会',
-        description: '2020年度講習会',
         created_at: '2020-02-12T08:01:37.838Z',
         updated_at: '2020-02-12T08:01:37.838Z'
       },
       {
         id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
         name: '2021講習会',
-        description: '2021年度講習会',
         created_at: '2021-02-12T08:01:37.838Z',
         updated_at: '2021-02-12T08:01:37.838Z'
       },
       {
         id: '3fa85f64-5717-4562-b3fc-2c963f66afa7',
         name: '2022講習会',
-        description: '2022年度講習会',
         created_at: '2022-02-12T08:01:37.838Z',
         updated_at: '2022-02-12T08:01:37.838Z'
       }
@@ -39,14 +36,7 @@ export const useTagStore = defineStore('tag', {
       const response: TagResponse = await axios.get('/api/tags/' + id)
       this.tag = response
     },
-    async postTag(tag: Tag) {
-      await axios.post('/api/tags', tag)
-      this.getTags()
-    },
-    async putTag(id: string, tag: Tag) {
-      await axios.put('/api/tags/' + id, tag)
-      this.getTags()
-    },
+
     async deleteTag(id: string) {
       await axios.delete('/api/tags' + id)
       this.getTags()

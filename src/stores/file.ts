bda89c19b2d8a1b1bf3a2635e3afa308c6b376de
mehm8128 <mehm8128@gmail.com>
2022-04-06 23:24:18 +0900

    fix file api

--- src/stores/file.ts
+++ src/stores/file.ts
@@ -1,33 +1,30 @@
-import axios from 'axios'
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-import { FileResponse } from '../types/fileTypes'
+import apis from '/@/lib/apis'
 
-export const useFileStore = defineStore('file', {
-  state: () => ({
-    files: new Array<FileResponse>()
-  }),
-  actions: {
-    async getFile(ids: string[]) {
-      for (let i = 0; i < ids.length; i++) {
-        const response: FileResponse = await axios.get('/api/files/' + ids[i])
-        this.files.concat(response)
-      }
-    },
-    async postFile(request_id: string, name: string, file: string) {
-      const formData = new FormData()
-      formData.append('file', file)
-      formData.append('name', name)
-      formData.append('request_id', request_id)
-      const header = {
-        headers: {
-          'Content-Type': 'mutipart/form-data'
-        }
-      }
-      await axios.post('/api/files/', formData, header)
-    },
-    async deleteFile(id: string) {
-      await axios.put('/api/files/' + id)
+export const useFileStore = defineStore('file', () => {
+  const files = ref<any[]>([])
+
+  const fetchFiles = async (ids: string[]) => {
+    for (let i = 0; i < ids.length; i++) {
+      files.value.concat((await apis.filesFileIDGet(ids[i])).data)
     }
   }
+  const postFile = async (request_id: string, name: string, file: string) => {
+    // const formData = new FormData()
+    // formData.append('file', file)
+    // formData.append('name', name)
+    // formData.append('request_id', request_id)
+    // const header = {
+    //   headers: {
+    //     'Content-Type': 'mutipart/form-data'
+    //   }
+    // }
+    await apis.filesPost(file, name, request_id)
+  }
+  const deleteFile = async (id: string) => {
+    await apis.filesFileIDDelete(id)
+  }
+  return { files, fetchFiles, postFile, deleteFile }
 })

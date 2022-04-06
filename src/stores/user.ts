0417198a30e91c4a5127d5660e71e22fce4447cb
mehm8128 <mehm8128@gmail.com>
2022-04-06 22:12:56 +0900

    fix user api

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,48 +1,29 @@
-import axios from 'axios'
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
-import { User, UserResponse } from '../types/userTypes'
+import type { User, PostUser } from '/@/lib/apis'
+import apis from '/@/lib/apis'
 
-export const useUserStore = defineStore('user', {
-  state: () => ({
-    me: {
-      name: 'mehm8128',
-      display_name: 'mehm8128',
-      admin: true,
-      created_at: '2022-01-27T13:45:37.048Z',
-      updated_at: '2022-01-27T13:45:37.048Z',
-      deleted_at: '2022-01-27T13:45:37.048Z'
-    }, //{} as UserResponse
-    users: [
-      {
-        name: 'nagatech',
-        display_name: 'ながてち',
-        admin: true,
-        created_at: '2022-01-25T13:45:37.048Z',
-        updated_at: '2022-01-25T13:45:37.048Z',
-        deleted_at: '2022-01-25T13:45:37.048Z'
-      },
-      {
-        name: 'mehm8128',
-        display_name: 'mehm8128',
-        admin: false,
-        created_at: '2022-01-27T13:45:37.048Z',
-        updated_at: '2022-01-27T13:45:37.048Z',
-        deleted_at: '2022-01-27T13:45:37.048Z'
-      }
-    ] //new Array<UserResponse>()
-  }),
-  actions: {
-    async getMe() {
-      const response: UserResponse = await axios.get('/api/users/me')
-      this.me = response
-    },
-    async getUsers() {
-      const response: UserResponse[] = await axios.get('/api/users')
-      this.users = response
-    },
-    async putUser(user: User) {
-      await axios.put('/api/users', user)
-    }
+export const useUserStore = defineStore('user', () => {
+  const me = ref<User>({
+    name: 'mehm8128',
+    display_name: 'mehm8128',
+    admin: true,
+    created_at: '2022-01-27T13:45:37.048Z',
+    updated_at: '2022-01-27T13:45:37.048Z',
+    deleted_at: '2022-01-27T13:45:37.048Z'
+  })
+  const users = ref<User[]>([])
+
+  const fetchMe = async () => {
+    me.value = (await apis.usersMeGet()).data
   }
+  const fetchUsers = async () => {
+    users.value = (await apis.usersGet()).data
+  }
+  const putUser = async (user: PostUser) => {
+    await apis.usersPut(user)
+  }
+
+  return { me, users, fetchMe, fetchUsers, putUser }
 })

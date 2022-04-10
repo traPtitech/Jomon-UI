06ba57901cd198fb96c55b7cd7d5f2d83efc80e2
mehm8128 <mehm8128@gmail.com>
2022-04-10 12:04:05 +0900

    fix store

--- src/stores/transaction.ts
+++ src/stores/transaction.ts
@@ -1,15 +1,13 @@
 import axios from 'axios'
 import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
 
-import {
-  Params,
-  Transaction,
-  TransactionRequest
-} from '../types/transactionTypes'
+import type { Transaction, PostTransaction } from '/@/lib/apis'
+import apis from '/@/lib/apis'
 
-export const useTransactionStore = defineStore('transaction', {
-  state: () => ({
-    transactions: Array(50).fill({
+export const useTransactionStore = defineStore('transaction', () => {
+  const transactions = ref<Transaction[]>([
+    {
       id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
       amount: 1200,
       target: 'hoge株式会社',
@@ -68,73 +66,70 @@ export const useTransactionStore = defineStore('transaction', {
       },
       created_at: '2022-02-09T14:03:53.278Z',
       updated_at: '2022-02-09T14:03:53.278Z'
-    }),
-    params: {
+    }
+  ])
+  const params = ref({
+    sort: 'created_at',
+    request: '',
+    target: '',
+    since: '',
+    until: '',
+    tag: '',
+    group: ''
+  })
+  const isTargetSearchOpen = ref<boolean>(false)
+
+  const transactionsLength = computed(() => {
+    return transactions.value.length
+  })
+  const transactionsFilter = (index: number) => {
+    return transactions.value.slice((index - 1) * 10, index * 10)
+  }
+
+  const getTransactions = async () => {
+    // for (let i = 0; i < this.tagList.length; i++) {
+    //   if (i === 0) {
+    //     this.params.tag = this.tagList[i]
+    //   } else {
+    //     this.params.tag += ',' + this.tagList[i]
+    //   }
+    // }
+    transactions.value = (
+      await apis.getRequests(
+        params.value.sort,
+        params.value.target,
+        params.value.since,
+        params.value.until,
+        params.value.tag,
+        params.value.group,
+        params.value.request
+      )
+    ).data
+  }
+  const postTransaction = async (transaction: PostTransaction) => {
+    const res = (await apis.postTransaction(transaction)).data
+    transactions.value.unshift(res)
+  }
+  function resetParams() {
+    params.value = {
       sort: 'created_at',
+      request: '',
       target: '',
-      year: null,
       since: '',
       until: '',
-      tag: null,
-      group: null
-    } as Params,
-    // tagList: new Array<string>(),
-    isTargetSearchOpen: false
-  }),
-  getters: {
-    transactionsLength() {
-      return () => this.transactions.length
-    },
-    transactionsFilter() {
-      return (index: number) => {
-        return this.transactions.slice((index - 1) * 13, index * 13)
-      }
-    },
-    dateFormatter() {
-      return (date: string) => {
-        return (
-          date.split('-')[0] +
-          '年' +
-          date.split('-')[1] +
-          '月' +
-          date.split('-')[2].split('T')[0] +
-          '日'
-        )
-      }
+      tag: '',
+      group: ''
     }
-  },
-  actions: {
-    async getTransactions() {
-      // for (let i = 0; i < this.tagList.length; i++) {
-      //   if (i === 0) {
-      //     this.params.tag = this.tagList[i]
-      //   } else {
-      //     this.params.tag += ',' + this.tagList[i]
-      //   }
-      // }
-      const response: Transaction[] = await axios.get('/api/transactions', {
-        params: this.params
-      })
-      this.transactions = response
-    },
-    async postTransaction(transaction: TransactionRequest) {
-      const response: Transaction = await axios.post(
-        '/api/transactions',
-        transaction
-      )
-      this.transactions.unshift(response)
-    },
-    resetParams() {
-      this.params = {
-        sort: 'created_at',
-        request: null,
-        target: null,
-        year: null,
-        since: '',
-        until: '',
-        tag: null,
-        group: null
-      }
-    }
+  }
+
+  return {
+    transactions,
+    transactionsLength,
+    transactionsFilter,
+    isTargetSearchOpen,
+    params,
+    getTransactions,
+    postTransaction,
+    resetParams
   }
 })

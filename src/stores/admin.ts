c821f2ccfe42fd566acb0896ea9e50cfddcc7e55
mehm8128 <mehm8128@gmail.com>
2022-04-11 15:46:07 +0900

    fix

--- src/stores/admin.ts
+++ src/stores/admin.ts
@@ -4,18 +4,20 @@ import { ref } from 'vue'
 import apis from '/@/lib/apis'
 
 export const useAdminStore = defineStore('admin', () => {
-  // admins: ['mehm8128', 'mehm81', 'mehm']
   const admins = ref<string[]>([])
   const isAdminFetched = ref(false)
 
   const fetchAdmins = async () => {
     admins.value = (await apis.getAdmins()).data
+    isAdminFetched.value = true
   }
   const postAdmins = async (ids: string[]) => {
     await apis.postAdmins(ids)
+    admins.value = admins.value.concat(ids)
   }
   const deleteAdmins = async (ids: string[]) => {
     await apis.deleteAdmins(ids)
+    admins.value = admins.value.filter(id => !ids.includes(id))
   }
   return {
     admins,

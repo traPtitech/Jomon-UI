35d836691b3ae9ae66f5d7e581fce52cbc095b97
mehm8128 <mehm8128@gmail.com>
2022-04-12 14:34:00 +0900

    fix

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,8 @@ const { me } = storeToRefs(userStore)
 </script>
 
 <template>
+  <Header :me="me" />
   <main class="h-screen text-dark-500">
-    <Header :me="me" />
-
     <router-view />
   </main>
 </template>

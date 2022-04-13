d7f7dcffd16ec5ceea4cc3b59dd3ba2d4b953e96
mehm8128 <mehm8128@gmail.com>
2022-04-13 22:51:05 +0900

    fix groups page

--- src/pages/Groups.vue
+++ src/pages/Groups.vue
@@ -30,41 +30,36 @@ watch(
 </script>
 
 <template>
+  <div class="flex my-4 justify-center items-center relative">
+    <h1 class="text-center text-3xl">グループ一覧</h1>
+    <div class="right-20 absolute">
+      <router-link to="/groups/new">
+        <Button font-size="lg" padding="md"> グループの新規作成 </Button>
+      </router-link>
+    </div>
+  </div>
+  <!--フィルタリングメニューあってもいい気がする-->
+  <div class="h-150">
+    <div class="w-2/3 mr-auto ml-auto border border-zinc-400">
+      <div class="flex justify-around items-center bg-gray-200 pt-2 pb-2 px-4">
+        <div class="w-1/5">グループ名</div>
+        <div class="w-3/5">詳細</div>
+        <div class="w-1/5">予算</div>
+      </div>
+      <div
+        class="w-full bg-zinc-400 border border-solid border-zinc-400 mr-auto ml-auto" />
+      <ul class="divide-y">
+        <li v-for="group in groupStore.groupsFilter(pageIndex)" :key="group.id">
+          <Group :group="group" />
+        </li>
+      </ul>
+    </div>
+  </div>
   <div>
-    <div class="flex my-4 justify-center items-center relative">
-      <h1 class="text-center text-3xl">グループ一覧</h1>
-      <div class="right-20 absolute">
-        <router-link to="/groups/new">
-          <Button font-size="lg" padding="md"> グループの新規作成 </Button>
-        </router-link>
-      </div>
-    </div>
-    <!--フィルタリングメニューあってもいい気がする-->
-    <div class="h-150">
-      <div class="w-2/3 mr-auto ml-auto border border-zinc-400">
-        <div
-          class="flex justify-around items-center bg-gray-200 pt-2 pb-2 px-4">
-          <div class="w-1/5">グループ名</div>
-          <div class="w-3/5">詳細</div>
-          <div class="w-1/5">予算</div>
-        </div>
-        <div
-          class="w-full bg-zinc-400 border border-solid border-zinc-400 mr-auto ml-auto" />
-        <ul class="divide-y">
-          <li
-            v-for="group in groupStore.groupsFilter(pageIndex)"
-            :key="group.id">
-            <Group :group="group" />
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div>
-      <PaginationBar
-        :item-length="groupStore.groups.length"
-        kind="groups"
-        :page-index="pageIndex"
-        :unit="10" />
-    </div>
+    <PaginationBar
+      :item-length="groupStore.groups.length"
+      kind="groups"
+      :page-index="pageIndex"
+      :unit="10" />
   </div>
 </template>

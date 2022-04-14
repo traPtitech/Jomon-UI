c170a6121b4b867468bbbb051a7dc2f3b28b9770
mehm8128 <mehm8128@gmail.com>
2022-04-14 14:24:30 +0900

    fix new group page

--- src/pages/NewGroup.vue
+++ src/pages/NewGroup.vue
@@ -35,18 +35,18 @@ async function handlePostGroup() {
 </script>
 
 <template>
-  <div class="pt-8 px-12 h-full">
+  <div class="pt-8 px-60 h-full">
     <h1 class="text-3xl text-center">グループの新規作成</h1>
     <div class="flex flex-col justify-between mt-8 ml-12 mr-12 text-xl h-4/5">
       <div class="flex flex-col">
         <label>グループ名</label>
-        <input v-model="group.name" class="border border-gray-300 w-2/5" />
+        <input v-model="group.name" class="border border-gray-300" />
       </div>
       <div class="flex flex-col">
         <label>詳細</label>
         <textarea
           v-model="group.description"
-          class="border border-gray-300 resize-none w-2/5" />
+          class="border border-gray-300 resize-none" />
       </div>
       <div class="flex flex-col">
         <label>予算</label>
@@ -60,7 +60,7 @@ async function handlePostGroup() {
         <label>オーナー</label>
         <VueSelect
           v-model="group.owners"
-          class="w-1/2"
+          class="w-4/5"
           :close-on-select="false"
           label="name"
           multiple
@@ -73,7 +73,7 @@ async function handlePostGroup() {
         <label>メンバー</label>
         <VueSelect
           v-model="group.members"
-          class="w-1/2"
+          class="w-4/5"
           :close-on-select="false"
           label="name"
           multiple

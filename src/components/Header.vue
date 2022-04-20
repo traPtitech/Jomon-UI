d1213d1294c5d20c06c40e1c00ffef50d5ad4d13
mehm8128 <mehm8128@gmail.com>
2022-04-20 20:13:50 +0900

    fix header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { useRoute } from 'vue-router'
 
+import HeaderButton from './HeaderButton.vue'
 import Logo from './shared/JomonLogo.vue'
 import UserIcon from './shared/UserIcon.vue'
 import type { User } from '/@/lib/apis'
@@ -14,61 +15,28 @@ defineProps<Props>()
 </script>
 
 <template>
-  <header class="flex h-12 shadow w-full items-center fixed">
+  <header class="flex h-12 shadow w-full items-center fixed bg-white z-9999">
     <router-link to="/">
       <Logo />
     </router-link>
     <div class="flex h-full flex-1 px-2 justify-between">
       <div class="flex gap-2 items-center">
-        <router-link
-          :class="route.fullPath === '/requests' ? 'cursor-default' : ''"
-          to="/requests">
-          <div
-            class="p-2 rounded-3xl"
-            :class="
-              route.fullPath === '/requests'
-                ? 'bg-zinc-200'
-                : 'hover:bg-zinc-200'
-            ">
-            <span>申請一覧</span>
-          </div>
-        </router-link>
-        <router-link
-          :class="route.fullPath === '/transactions' ? 'cursor-default' : ''"
-          to="/transactions">
-          <div
-            class="p-2 rounded-3xl"
-            :class="
-              route.fullPath === '/transactions'
-                ? 'bg-zinc-200'
-                : 'hover:bg-zinc-200'
-            ">
-            <span>入出金記録一覧</span>
-          </div>
-        </router-link>
-        <router-link
-          :class="route.fullPath === '/groups' ? 'cursor-default' : ''"
-          to="/groups">
-          <div
-            class="p-2 rounded-3xl"
-            :class="
-              route.fullPath === '/groups' ? 'bg-zinc-200' : 'hover:bg-zinc-200'
-            ">
-            <span>グループ一覧</span>
-          </div>
-        </router-link>
-        <router-link
-          v-if="me.admin"
-          :class="route.fullPath === '/admins' ? 'cursor-default' : ''"
-          to="/admins">
-          <div
-            class="p-2 rounded-3xl"
-            :class="
-              route.fullPath === '/admins' ? 'bg-zinc-200' : 'hover:bg-zinc-200'
-            ">
-            <span>管理ページ</span>
-          </div>
-        </router-link>
+        <HeaderButton
+          :fullpath="route.fullPath"
+          path="/requests"
+          text="申請一覧" />
+        <HeaderButton
+          :fullpath="route.fullPath"
+          path="/transactions"
+          text="入出金記録一覧" />
+        <HeaderButton
+          :fullpath="route.fullPath"
+          path="/groups"
+          text="グループ一覧" />
+        <HeaderButton
+          :fullpath="route.fullPath"
+          path="/admins"
+          text="管理ページ" />
       </div>
       <UserIcon v-if="me.name" :name="me.name" />
     </div>
---
+++ src/components/HeaderButton.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+interface Props {
+  fullpath: string
+  path: string
+  text: string
+}
+defineProps<Props>()
+</script>
+<template>
+  <router-link :class="fullpath === path ? 'cursor-default' : ''" :to="path">
+    <div
+      class="p-2 rounded-3xl"
+      :class="fullpath === path ? 'bg-zinc-200' : 'hover:bg-zinc-200'">
+      <span>{{ text }}</span>
+    </div>
+  </router-link>
+</template>

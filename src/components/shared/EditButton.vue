92cbcf6361ae1a69a5871fa9e24c3e381e2bb600
mehm8128 <mehm8128@gmail.com>
2022-07-21 15:26:18 +0900

    add toRequest button

---
+++ src/components/shared/EditButton.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { PencilIcon } from '@heroicons/vue/solid'
+</script>
+
+<template>
+  <button class="inline-block">
+    <PencilIcon class="h-5 w-5" />
+  </button>
+</template>

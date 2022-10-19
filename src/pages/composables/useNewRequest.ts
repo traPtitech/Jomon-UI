6c010af20c9401cc8e66bd227c255b00654cb2bd
mehm8128 <mehm8128@gmail.com>
2022-10-19 12:49:43 +0900

    divide composable

--- src/pages/composables/useNewRequest.ts
+++ src/pages/composables/useNewRequest.ts
@@ -1,14 +1,14 @@
-import type { AxiosResponse } from 'axios'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useToast } from 'vue-toastification'
 
 import { useRequestStore } from '/@/stores/request'
-import { useTagStore } from '/@/stores/tag'
 import { useUserStore } from '/@/stores/user'
 
-import type { Request, Tag } from '/@/lib/apis'
+import type { Request } from '/@/lib/apis'
 import apis from '/@/lib/apis'
+
+import { usePostTags } from './usePostTags'
 
 export interface FileRequest {
   name: string
@@ -28,9 +28,9 @@ export interface RequestRequest {
 export const useNewRequest = () => {
   const toast = useToast()
   const router = useRouter()
-  const tagStore = useTagStore()
   const requestStore = useRequestStore()
   const userStore = useUserStore()
+  const { postTags } = usePostTags()
 
   const request = ref<RequestRequest>({
     created_by: userStore.me?.name ?? '',
@@ -56,30 +56,12 @@ export const useNewRequest = () => {
       toast.warning('タイトル、内容、対象者は必須です')
       return
     }
-    const postTagResults: ReturnType<typeof apis.postTag>[] = []
-    let tags: string[] = []
-    // 以下の2パターンで場合分けしていて、条件式が1でtrue、2でfalseになる
-    // 1. willPutRequest.tagsに入っているidがtagStore.tagsのタグのidと一致する
-    // 2. タグが新規作成されて、そのタグ名がwillPutRequest.tagsに入っている
-    // 2の場合にはタグ名がtagStore.tagsのタグidと一致することはないので、tagPostPromisesにpostTag関数がpushされる
-    request.value.tags.forEach((tag: string) => {
-      if (tagStore.tags?.some(t => t.id === tag)) {
-        tags.push(tag)
-        return
-      }
-      postTagResults.push(apis.postTag({ name: tag }))
-    })
+    let tags
     try {
-      tags = tags.concat(
-        (await Promise.all(postTagResults)).map(
-          (tag: AxiosResponse<Tag>) => tag.data.id
-        )
-      )
+      tags = await postTags(request.value.tags)
     } catch {
-      toast.error('申請の作成に失敗しました')
       return
     }
-
     const requestRequest = {
       ...request.value,
       tags: tags,
--- src/pages/composables/useRequestDetail.ts
+++ src/pages/composables/useRequestDetail.ts
@@ -1,13 +1,12 @@
-import type { AxiosResponse } from 'axios'
 import { storeToRefs } from 'pinia'
 import { ref } from 'vue'
 import { useToast } from 'vue-toastification'
 
 import { useRequestDetailStore } from '/@/stores/requestDetail'
-import { useTagStore } from '/@/stores/tag'
 
-import type { Tag } from '/@/lib/apis'
 import apis from '/@/lib/apis'
+
+import { usePostTags } from './usePostTags'
 
 export type EditMode =
   | 'title'
@@ -30,8 +29,8 @@ export interface EditedValue {
 
 export const useRequestDetail = () => {
   const requestDetailStore = useRequestDetailStore()
-  const tagStore = useTagStore()
   const toast = useToast()
+  const { postTags } = usePostTags()
   const { request } = storeToRefs(requestDetailStore)
 
   const editMode = ref<EditMode>('')
@@ -54,28 +53,12 @@ export const useRequestDetail = () => {
     }
     editMode.value = ''
   }
+
   const putRequest = async (id: string, willPutRequest: EditedValue) => {
-    const tagPostPromises: ReturnType<typeof apis.postTag>[] = []
-    let tags: string[] = []
-    // 以下の2パターンで場合分けしていて、条件式が1でtrue、2でfalseになる
-    // 1. willPutRequest.tagsに入っているidがtagStore.tagsのタグのidと一致する
-    // 2. タグが新規作成されて、そのタグ名がwillPutRequest.tagsに入っている
-    // 2の場合にはタグ名がtagStore.tagsのタグidと一致することはないので、tagPostPromisesにpostTag関数がpushされる
-    willPutRequest.tags.forEach((tag: string) => {
-      if (tagStore.tags?.some(t => t.id === tag)) {
-        tags.push(tag)
-        return
-      }
-      tagPostPromises.push(apis.postTag({ name: tag }))
-    })
+    let tags
     try {
-      tags = tags.concat(
-        (await Promise.all(tagPostPromises)).map(
-          (tag: AxiosResponse<Tag>) => tag.data.id
-        )
-      )
+      tags = await postTags(willPutRequest.tags)
     } catch {
-      toast.error('申請の修正に失敗しました')
       return
     }
     const putRequest = {

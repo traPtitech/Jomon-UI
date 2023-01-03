00b70bf37df48b17caf87c305fa23543851f927c
mehm8128 <mehm8128@gmail.com>
2023-01-03 23:12:58 +0900

    refactor new group

--- src/pages/NewGroupPage.vue
+++ src/pages/NewGroupPage.vue
@@ -1,14 +1,7 @@
 <script lang="ts" setup>
 import { storeToRefs } from 'pinia'
-import { reactive } from 'vue'
-import { useRouter } from 'vue-router'
-import { useToast } from 'vue-toastification'
 
-import { useGroupStore } from '/@/stores/group'
 import { useUserStore } from '/@/stores/user'
-
-import type { Group, PostGroup } from '/@/lib/apis'
-import apis from '/@/lib/apis'
 
 import InputNumber from '/@/components/shared/InputNumber.vue'
 import InputSelectMultiple from '/@/components/shared/InputSelectMultiple.vue'
@@ -16,62 +9,13 @@ import InputText from '/@/components/shared/InputText.vue'
 import InputTextarea from '/@/components/shared/InputTextarea.vue'
 import SimpleButton from '/@/components/shared/SimpleButton.vue'
 
-const router = useRouter()
+import { useNewGroup } from './composables/useNewGroup'
 
 const userStore = useUserStore()
-const groupStore = useGroupStore()
 const { fetchUsers } = userStore
-const { me, isUserFetched, userOptions } = storeToRefs(userStore)
-const { groups } = storeToRefs(groupStore)
+const { isUserFetched, userOptions } = storeToRefs(userStore)
 
-const toast = useToast()
-
-const group = reactive({
-  name: '',
-  description: '',
-  budget: 0,
-  owners: me.value?.id ? [me.value.id] : [],
-  members: []
-})
-
-const postGroup = async (group: PostGroup) => {
-  try {
-    const res = (await apis.postGroup(group)).data
-    if (groups.value !== undefined) {
-      groups.value.unshift(res)
-    } else {
-      groups.value = [res]
-    }
-    return res
-  } catch {
-    toast.error('グループの作成に失敗しました')
-    throw new Error('グループの作成に失敗しました')
-  }
-}
-
-const handlePostGroup = async (e: Event) => {
-  e.preventDefault()
-  if (group.owners.length === 0) {
-    toast.warning('オーナーは必須です')
-    return
-  }
-  const willPostGroup = {
-    name: group.name,
-    description: group.description,
-    budget: group.budget
-  }
-  try {
-    const res: Group = await postGroup(willPostGroup)
-    await Promise.all([
-      apis.postGroupMembers(res.id, group.members),
-      apis.postGroupOwners(res.id, group.owners)
-    ])
-    toast.success('グループを作成しました')
-    router.push(`/groups/${res.id}`)
-  } catch {
-    throw new Error('グループの作成に失敗しました')
-  }
-}
+const { group, postGroup } = useNewGroup()
 
 if (!isUserFetched.value) {
   await fetchUsers()
@@ -83,7 +27,7 @@ if (!isUserFetched.value) {
     <div class="py-8">
       <h1 class="text-center text-3xl">グループの新規作成</h1>
     </div>
-    <form class="flex flex-col gap-2" @submit="handlePostGroup">
+    <form class="flex flex-col gap-2">
       <div class="flex flex-col">
         <label>グループ名</label>
         <InputText
@@ -127,7 +71,7 @@ if (!isUserFetched.value) {
           class="ml-auto mt-8 block"
           font-size="xl"
           padding="md"
-          @click="handlePostGroup">
+          @click="postGroup">
           グループを作成する
         </SimpleButton>
       </div>

85ba6b70ecc17b2f83e22424b65c9dc00fa7e5b3
rei <h.watanabe360@gmail.com>
2024-11-02 14:23:38 +0900

    eslint.config.jsにPrettier設定を追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import pluginJs from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import pluginVue from 'eslint-plugin-vue'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
@@ -8,13 +9,20 @@ export default [
   {
     ignores: ['**/dist/**']
   },
-  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,vue}']
+  },
+  {
+    languageOptions: { globals: globals.browser }
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   {
     files: ['**/*.vue'],
-    languageOptions: { parserOptions: { parser: tseslint.parser } }
-  }
+    languageOptions: {
+      parserOptions: { parser: tseslint.parser }
+    }
+  },
+  eslintConfigPrettier
 ]

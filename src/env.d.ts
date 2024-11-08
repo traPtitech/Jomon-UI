b1bdc139935ec6afd3a2e93c858468f2afe27ed5
rei <h.watanabe360@gmail.com>
2024-11-08 23:21:32 +0900

    🐛 fix: gen-apiが正常に走るように

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,11 +2,8 @@
 
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<
-    Record<string, unknown>,
-    Record<string, unknown>,
-    unknown
-  >
+  // eslint-disable-next-line @typescript-eslint/ban-types @typescript-eslint/no-explicit-any
+  const component: DefineComponent<{}, {}, any>
   export default component
 }
 

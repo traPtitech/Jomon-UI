ebcce71c5d917a69bf94d5f557ea871deea22dca
anko9801 <daiusa3@icloud.com>
2024-12-29 23:23:08 +0900

    🎸 feat: traP からどこへお金が流れていくかを表示するjk:w

--- src/components/transactionDetail/TransactionDetail.vue
+++ src/components/transactionDetail/TransactionDetail.vue
@@ -3,6 +3,7 @@ import { computed } from 'vue'
 
 import { formatDate } from '/@/lib/date'
 
+import { ArrowLongRightIcon } from '@heroicons/vue/24/outline'
 import TagsGroup from '/@/components/shared/TagsGroup.vue'
 import type { Transaction } from '/@/features/transaction/model'
 
@@ -15,14 +16,24 @@ const formattedDate = computed(() => formatDate(props.transaction.createdAt))
 </script>
 
 <template>
-  <div class="flex flex-col gap-3">
+  <div class="flex flex-col gap-6">
     <div class="flex flex-col gap-3">
       <h2 class="text-xl">年月日</h2>
       <p>{{ formattedDate }}</p>
     </div>
     <div class="flex flex-col gap-3">
       <h2 class="text-xl">入出金</h2>
-      <p>{{ Math.abs(transaction.amount) }}円</p>
+      <div>
+        <p>{{ Math.abs(transaction.amount) }}円</p>
+        <div
+          :class="`flex items-center gap-4 ${
+            transaction.amount < 0 ? 'flex-row-reverse justify-end' : 'flex-row'
+          }`">
+          traP
+          <ArrowLongRightIcon class="w-8" />
+          {{ transaction.target }}
+        </div>
+      </div>
     </div>
     <div class="flex flex-col gap-3">
       <h2 class="text-xl">取引グループ</h2>

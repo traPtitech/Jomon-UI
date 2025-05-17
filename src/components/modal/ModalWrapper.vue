e5133e2eeff8810acf963245a131639b663f198a
anko9801 <daiusa3@icloud.com>
2025-05-17 10:39:01 +0900

    ♿️ モーダルを閉じる UI に a11y を付与

--- src/components/modal/ModalWrapper.vue
+++ src/components/modal/ModalWrapper.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 const emit = defineEmits<(e: 'closeModal') => void>()
+
+const handleKeydown = (e: KeyboardEvent) => {
+  if (e.key === 'Enter' || e.key === ' ') {
+    emit('closeModal')
+  }
+}
 </script>
 
 <template>
   <teleport to="body">
     <div
+      role="button"
+      tabindex="0"
+      aria-label="Close modal"
       className="z-10 fixed top-0 left-0 h-full w-full bg-surface-secondary/50"
-      @click.self="emit('closeModal')">
+      @click.self="emit('closeModal')"
+      @keydown="handleKeydown">
       <slot />
     </div>
   </teleport>

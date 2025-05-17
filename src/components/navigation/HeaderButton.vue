0920559e5b72e4ac8884c8f4b5c85f9fad2d2c04
anko9801 <daiusa3@icloud.com>
2025-05-17 10:50:48 +0900

    ♿️ ヘッダーのボタンにフォーカスできるようにする

--- src/components/navigation/HeaderButton.vue
+++ src/components/navigation/HeaderButton.vue
@@ -9,7 +9,11 @@ const props = defineProps<Props>()
 </script>
 
 <template>
-  <router-link :class="props.isHere ? 'cursor-default' : ''" :to="props.path">
+  <router-link 
+    :class="props.isHere ? 'cursor-default' : ''"
+    :to="props.path"
+    :aria-current="props.isHere ? 'page' : undefined"
+    tabindex="0">
     <div
       class="rounded-3xl p-2"
       :class="props.isHere ? 'bg-hover-primary' : 'hover:bg-hover-primary'">

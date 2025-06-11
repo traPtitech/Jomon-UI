6025ea79937f968603cd8dcaabe9e7410cbc6412
Daiki Usami <d_usami@no9.co.jp>
2025-06-11 13:40:12 +0900

    feat: .env ファイルでバックエンドのドメインを指定

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,31 @@ import vue from '@vitejs/plugin-vue'
 import * as https from 'https'
 import * as path from 'path'
 import brotli from 'rollup-plugin-brotli'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-const keepAliveAgent = new https.Agent({ keepAlive: true })
-const DEV_SERVER_PROXY_HOST = 'https://jomon-dev.trapti.tech'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const keepAliveAgent = new https.Agent({ keepAlive: true })
 
-export default defineConfig({
-  plugins: [vue(), brotli(), tailwindcss()],
-  resolve: {
-    alias: {
-      '/@': path.resolve(__dirname, '/src')
-    }
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api/*': {
-        target: DEV_SERVER_PROXY_HOST,
-        changeOrigin: true,
-        agent: keepAliveAgent
+  return {
+    plugins: [vue(), brotli(), tailwindcss()],
+    resolve: {
+      alias: {
+        '/@': path.resolve(__dirname, '/src')
       }
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        '/api/*': {
+          target: env.DEV_SERVER_PROXY_HOST,
+          changeOrigin: true,
+          agent: keepAliveAgent
+        }
+      }
+    },
+    optimizeDeps: {
+      include: ['axe-core']
     }
-  },
-  optimizeDeps: {
-    include: ['axe-core']
   }
 })

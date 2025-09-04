2f50bce3fb9081d7375b08e5e29f27348732605e
SAH123 <shota.himeji@m.ieice.org>
2025-09-04 16:13:06 +0900

    Escapeを押すことでモーダルを閉じられるように

--- src/components/modal/ModalWrapper.vue
+++ src/components/modal/ModalWrapper.vue
@@ -1,32 +1,12 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
-
 const emit = defineEmits<(e: 'closeModal') => void>()
 
 const handleKeydown = (e: KeyboardEvent) => {
-  if (e.key === 'Enter' && isAnyFocused.value === false) {
+  if (e.key === 'Escape') {
     emit('closeModal')
     
   }
 }
-
-const isAnyFocused = ref(false)
-
-const updateFocusState = () => {
-  const active = document.activeElement
-  isAnyFocused.value = active && active.tagName === "TEXTAREA"
-}
-
-onMounted(() => {
-  window.addEventListener("focusin", updateFocusState)
-  window.addEventListener("focusout", updateFocusState)
-})
-
-onUnmounted(() => {
-  window.removeEventListener("focusin", updateFocusState)
-  window.removeEventListener("focusout", updateFocusState)
-})
-
 </script>
 
 <template>

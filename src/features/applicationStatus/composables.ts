ba2bd49ad46c3fc843e55ec76ad72930c6ac479b
rei <107461411+reiroop@users.noreply.github.com>
2025-09-09 12:18:38 +0900

    refactor: useStatusOptionsをStatusEnumに合わせてrename

--- src/features/applicationStatus/composables.ts
+++ src/features/applicationStatus/composables.ts
@@ -11,14 +11,14 @@ export const useStatusOptions = (application: ApplicationDetail) => {
 
   const hasAuthority = isApplicationCreator.value(me.value)
 
-  const showToSubmitted =
+  const showToPendingReview =
     (hasAuthority && application.status === 'change_requested') ||
     (isAccountManager.value &&
       (application.status === 'change_requested' ||
         application.status === 'approved'))
-  const showToRequired =
+  const showToChangeRequested =
     isAccountManager.value && application.status === 'pending_review'
-  const showToAccepted =
+  const showToApproved =
     isAccountManager.value && application.status === 'pending_review'
   const showToRejected =
     isAccountManager.value && application.status === 'pending_review'
@@ -26,11 +26,19 @@ export const useStatusOptions = (application: ApplicationDetail) => {
   const statusOptions = computed<
     { value: ApplicationStatus; key: string; show: boolean }[]
   >(() => [
-    { value: 'pending_review', key: '承認待ちにする', show: showToSubmitted },
-    { value: 'change_requested', key: '要修正にする', show: showToRequired },
-    { value: 'approved', key: '承認する', show: showToAccepted },
+    {
+      value: 'pending_review',
+      key: '承認待ちにする',
+      show: showToPendingReview
+    },
+    {
+      value: 'change_requested',
+      key: '要修正にする',
+      show: showToChangeRequested
+    },
+    { value: 'approved', key: '承認する', show: showToApproved },
     { value: 'rejected', key: '却下する', show: showToRejected },
-    { value: 'payment_finished', key: '支払い済み', show: showToSubmitted }
+    { value: 'payment_finished', key: '支払い済み', show: showToPendingReview }
   ])
   const showStatusOptions = computed(() =>
     statusOptions.value

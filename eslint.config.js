f8903e87572e5dbdc46f72691b6e8f09e50f600a
rei <107461411+reiroop@users.noreply.github.com>
2025-09-21 18:13:48 +0900

    chore(eslint): ESLint設定を最新ベストプラクティスへ変更

--- eslint.config.js
+++ eslint.config.js
@@ -1,53 +1,68 @@
 import pluginJs from '@eslint/js'
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs
+} from '@vue/eslint-config-typescript'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVueA11y from 'eslint-plugin-vuejs-accessibility'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import globals from 'globals'
-import tseslint from 'typescript-eslint'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+const tsconfigRootDir = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig([
+  globalIgnores([
+    '**/dist/**',
+    'public/mockServiceWorker.js',
+    'src/lib/apis/generated/**'
+  ]),
   {
-    ignores: [
-      '**/dist/**',
-      'public/mockServiceWorker.js',
-      'src/lib/apis/generated/**'
-    ]
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,vue}']
-  },
-  {
+    files: ['src/**/*.{js,ts,tsx,vue}'],
     languageOptions: {
       globals: globals.browser
     }
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/recommended'],
   {
-    files: ['**/*.vue'],
-    languageOptions: {
-      parserOptions: {
-        parser: tseslint.parser
+    files: [
+      'eslint.config.js',
+      'vite.config.ts',
+      '**/*.config.{js,ts,mjs,cjs}',
+      'scripts/**/*.{js,ts,mts,cts}'
+    ],
+    languageOptions: { globals: globals.node }
+  },
+  pluginJs.configs.recommended,
+  ...defineConfigWithVueTs(
+    pluginVue.configs['flat/strongly-recommended'],
+    pluginVueA11y.configs['flat/recommended'],
+    {
+      files: ['**/*.{ts,tsx,cts,mts,vue}'],
+      languageOptions: {
+        parserOptions: {
+          projectService: true,
+          tsconfigRootDir
+        }
       }
     },
-    rules: {
-      'vue/require-default-prop': 'off'
-    }
-  },
-  ...pluginVueA11y.configs['flat/recommended'],
-  {
-    rules: {
-      'vuejs-accessibility/label-has-for': [
-        'error',
-        {
-          required: {
-            some: ['nesting', 'id']
+    vueTsConfigs.strictTypeChecked,
+    vueTsConfigs.stylisticTypeChecked,
+    {
+      files: ['**/*.vue'],
+      rules: {
+        'vue/require-default-prop': 'off',
+        'vuejs-accessibility/label-has-for': [
+          'error',
+          {
+            required: {
+              some: ['nesting', 'id']
+            }
           }
-        }
-      ]
+        ]
+      }
     }
-  },
+  ),
   eslintConfigPrettier
-]
+])

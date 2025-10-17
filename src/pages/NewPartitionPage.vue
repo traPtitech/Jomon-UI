211073501f446fa5d58b33e0253ecf5b949a10a4
sahara2006 <shouta.04077@gmail.com>
2025-10-17 16:31:17 +0900

    変数名称を変更

--- src/pages/NewPartitionPage.vue
+++ src/pages/NewPartitionPage.vue
@@ -12,14 +12,14 @@ const { isSending, partition, handleCreatePartition } = useNewPartition()
 if (!isUserFetched.value) {
   await fetchUsers()
 }
-const nolimitpartition = ref(false)
+const noLimitPartition = ref(false)
 
 setInterval(() => {
-  console.log(nolimitpartition.value,partition.value.budget);
+  console.log(noLimitPartition.value,partition.value.budget);
 }, 1000);
 
 
-watch(nolimitpartition, (val) => {
+watch(noLimitPartition, (val) => {
   if (isRef(partition) && val) {
     partition.value.budget = 1000000000
   }
@@ -36,12 +36,12 @@ watch(nolimitpartition, (val) => {
   <form class="flex flex-col gap-6">
     <BaseInput v-model="partition.name" label="パーティション名" required />
     <div>
-    <BaseInput v-if="!nolimitpartition" v-model="partition.budget" type="number" label="予算" required :readonly="nolimitpartition"  :class="{ 'bg-gray-100 cursor-not-allowed': nolimitpartition }">
+    <BaseInput v-if="!noLimitPartition" v-model="partition.budget" type="number" label="予算" required :readonly="noLimitPartition"  :class="{ 'bg-gray-100 cursor-not-allowed': noLimitPartition }">
       <span class="mt-auto mb-2 ml-3 text-2xl font-bold text-text-secondary">
         ¥
       </span>
     </BaseInput>
-    <BaseInput v-model="nolimitpartition" type="checkbox" label="予算未定" />
+    <BaseInput v-model="noLimitPartition" type="checkbox" label="予算上限なし" />
    </div>
     <div>
       <SimpleButton

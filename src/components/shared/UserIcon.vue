94ac54ca79553fda5911721bbbe8eac5a7e05823
rei <107461411+reiroop@users.noreply.github.com>
2025-11-29 23:50:42 +0900

    fix(UserIcon): remove unused size prop and dynamic Tailwind classes

--- src/components/shared/UserIcon.vue
+++ src/components/shared/UserIcon.vue
@@ -1,24 +1,17 @@
 <script lang="ts" setup>
 import { UserCircleIcon } from '@heroicons/vue/24/solid'
 
-const props = withDefaults(
-  defineProps<{
-    size?: number
-    name?: string
-  }>(),
-  { size: 12, name: '' }
-)
+const props = defineProps<{
+  name?: string
+}>()
 </script>
 
 <template>
   <img
     v-if="props.name"
     :alt="props.name"
-    :class="`h-full rounded-full p-1 w-${props.size}`"
+    class="h-full rounded-full p-1"
     :src="`https://q.trap.jp/api/v3/public/icon/${props.name}`"
     :title="props.name" />
-  <UserCircleIcon
-    v-else
-    :class="`w-${props.size} h-${props.size} text-gray-400`"
-    title="不明なユーザー" />
+  <UserCircleIcon v-else class="text-gray-400" title="不明なユーザー" />
 </template>

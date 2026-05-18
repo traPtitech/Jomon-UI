f4111a8ec46cedb1e791c4887a07853bd5382526
SAH123 <shota.himeji@m.ieice.org>
2026-05-18 18:24:01 +0900

    iframeが特定のURLの場合表示されるように

--- src/components/applicationDetail/CommentLog.vue
+++ src/components/applicationDetail/CommentLog.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { computed } from 'vue'
+
 import { formatDateAndTime } from '@/lib/date'
 
 import MarkdownIt from '@/components/shared/MarkdownIt.vue'
@@ -9,6 +11,27 @@ import { useUserStore } from '@/features/user/store'
 const props = defineProps<{
   comment: ApplicationComment
 }>()
+
+const traqMatch = computed(() => {
+  const patterns = [
+    /^https:\/\/q\.trap\.jp\/messages\/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$/i,
+  ]
+
+  const text = props.comment.comment
+  for (const pattern of patterns) {
+    const match = text.match(pattern)
+    if (match) {
+      return match[1]
+    }
+  }
+
+  return null
+})
+
+const embedUrl = computed(() => {
+  if (!traqMatch.value) return null
+  return `https://q.trap.jp/widget/?type=message&id=${traqMatch.value}`
+})
 
 const formattedDateAndTime = formatDateAndTime(props.comment.createdAt)
 
@@ -34,7 +57,16 @@ const { getUserName, getUserNameWithFallback } = useUserStore()
       </time>
     </div>
     <div class="ml-16">
+      <iframe
+        v-if="embedUrl"
+        :src="embedUrl"
+        title="traq"
+        class="w-full rounded-lg"
+        scrolling="no"
+        frameborder="0">
+      </iframe>
       <MarkdownIt
+        v-else
         class="rounded-lg border border-surface-secondary px-4 py-3"
         :text="comment.comment" />
     </div>

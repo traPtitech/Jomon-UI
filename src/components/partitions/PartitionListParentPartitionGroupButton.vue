59b80cf5f5b82f32c5b97ec415048d658bc77544
SAH123 <shota.himeji@m.ieice.org>
2026-05-26 18:46:37 +0900

    format

--- src/components/partitions/PartitionListParentPartitionGroupButton.vue
+++ src/components/partitions/PartitionListParentPartitionGroupButton.vue
@@ -16,46 +16,52 @@ const { canEditPartition, editPartition } = usePartitionStore()
 const { me, fetchMe, isMeFetched } = useUserStore()
 
 const { partition } = defineProps<{
-    partition: Partition
+  partition: Partition
 }>()
 
 if (!isMeFetched.value) {
-    await fetchMe()
+  await fetchMe()
 }
 
 const editedParentPartitionGroup = ref(partition.parentPartitionGroupId)
 watch(
-    () => partition.parentPartitionGroupId,
-    newParentPartitionGroupId => {
-        if (!isEditMode.value) {
-            editedParentPartitionGroup.value = newParentPartitionGroupId
-        }
+  () => partition.parentPartitionGroupId,
+  newParentPartitionGroupId => {
+    if (!isEditMode.value) {
+      editedParentPartitionGroup.value = newParentPartitionGroupId
     }
+  }
 )
 
 const { isEditMode, isSaving, handleSave } = useEditor(
-    toRef(() => partition.parentPartitionGroupId),
-    editedParentPartitionGroup,
-    () =>
-        editPartition(partition.id, {
-            ...partition,
-            parentPartitionGroupId: editedParentPartitionGroup.value,
-        }),
-    '親パーティショングループの更新に失敗しました'
+  toRef(() => partition.parentPartitionGroupId),
+  editedParentPartitionGroup,
+  () =>
+    editPartition(partition.id, {
+      ...partition,
+      parentPartitionGroupId: editedParentPartitionGroup.value,
+    }),
+  '親パーティショングループの更新に失敗しました'
 )
 </script>
 
 <template>
-    <div v-if="isEditMode" class="flex gap-2">
-        <SearchSelect v-model="editedParentPartitionGroup" :options="partitionGroupOptions" required
-            label="親パーティショングループを編集" />
-        <button @click="handleSave" :disabled="isSaving">
-            <CheckIcon class="h-4 w-4 text-text-secondary hover:text-text-primary" />
-        </button>
-    </div>
-    <button v-else-if="canEditPartition(me)" @click="isEditMode = true"
-        class="flex cursor-pointer items-baseline gap-2">
-        <BriefcaseIcon class="h-3 w-3 text-text-secondary hover:text-text-primary" />
-        &nbsp;
+  <div v-if="isEditMode" class="flex gap-2">
+    <SearchSelect
+      v-model="editedParentPartitionGroup"
+      :options="partitionGroupOptions"
+      required
+      label="親パーティショングループを編集" />
+    <button @click="handleSave" :disabled="isSaving">
+      <CheckIcon class="h-4 w-4 text-text-secondary hover:text-text-primary" />
     </button>
+  </div>
+  <button
+    v-else-if="canEditPartition(me)"
+    @click="isEditMode = true"
+    class="flex cursor-pointer items-baseline gap-2">
+    <BriefcaseIcon
+      class="h-3 w-3 text-text-secondary hover:text-text-primary" />
+    &nbsp;
+  </button>
 </template>
